f7641728299e3a5e09a3baf0c4da72743e2fef68
0vector0 <0vector0@gmail.com>
2016-09-01 22:30:16 +0300

    add consumer dao

--- src/main/java/com/github/mykhalechko/wardrobe/dao/ConsumerDao.java
+++ src/main/java/com/github/mykhalechko/wardrobe/dao/ConsumerDao.java
@@ -2,13 +2,18 @@ package com.github.mykhalechko.wardrobe.dao;
 
 import com.github.mykhalechko.wardrobe.models.Consumer;
 
-import java.io.Serializable;
+import java.util.List;
 
+public interface ConsumerDao {
 
-public class ConsumerDao<E extends Serializable> extends AbstractDao<E> {
+    public void addConsumer(Consumer consumer);
 
-    public ConsumerDao() {
-        super();
-        setClazz((Class<E>) Consumer.class);
-    }
+    public void updateConsumer(Consumer consumer);
+
+    public void removeConsumer(int id);
+
+    public Consumer getConsumerById(int id);
+
+    public List<Consumer> liConsumers();
+
 }
