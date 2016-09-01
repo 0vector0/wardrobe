e273f86fad51a6ec2f94bdc5235a667b3dc6c5e4
0vector0 <0vector0@gmail.com>
2016-09-01 17:23:09 +0300

    add service layer for item

---
+++ src/main/java/com/github/mykhalechko/wardrobe/service/ItemServiceImpl.java
@@ -0,0 +1,39 @@
+package com.github.mykhalechko.wardrobe.service;
+
+
+import com.github.mykhalechko.wardrobe.dao.ItemDao;
+import com.github.mykhalechko.wardrobe.dao.ItemDaoImpl;
+import com.github.mykhalechko.wardrobe.models.Item;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemServiceImpl implements ItemSevice {
+
+    private ItemDao itemDao;
+
+    public ItemServiceImpl() {
+        this.itemDao = new ItemDaoImpl();
+    }
+
+    public void addItem(Item item) {
+        itemDao.addItem(item);
+    }
+
+    public void updateItem(Item item) {
+        itemDao.updateItem(item);
+    }
+
+    public void removeItem(int id) {
+        itemDao.removeItem(id);
+    }
+
+    public Item getItemById(int id) {
+        return itemDao.getItemById(id);
+    }
+
+    public List<Item> liItems() {
+        return itemDao.liItems();
+    }
+}
---
+++ src/main/java/com/github/mykhalechko/wardrobe/service/ItemSevice.java
@@ -0,0 +1,17 @@
+package com.github.mykhalechko.wardrobe.service;
+
+import com.github.mykhalechko.wardrobe.models.Item;
+
+import java.util.List;
+
+public interface ItemSevice {
+    public void addItem(Item item);
+
+    public void updateItem(Item item);
+
+    public void removeItem(int id);
+
+    public Item getItemById(int id);
+
+    public List<Item> liItems();
+}
