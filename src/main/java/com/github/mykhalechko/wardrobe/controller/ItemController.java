7f6290a5036e307b792f2c2166e25917efa9a3eb
0vector0 <0vector0@gmail.com>
2016-09-05 22:36:54 +0300

    add remove edit methods

--- src/main/java/com/github/mykhalechko/wardrobe/controller/ItemController.java
+++ src/main/java/com/github/mykhalechko/wardrobe/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,14 +24,38 @@ public class ItemController {
         return "items";
     }
 
-    @RequestMapping(value = "items/add", method = RequestMethod.POST)
+    @RequestMapping(value = "/items/add", method = RequestMethod.POST)
     public String addItem(@ModelAttribute("item") Item item) {
-//        if(item.getId() == 0){
-        this.itemService.addItem(item);
-//        } else {
-//            this.itemService.updateItem(item);
-//        }
+        System.out.println(item);
+        if (item.getId() == 0) {
+            this.itemService.addItem(item);
+        } else {
+            this.itemService.updateItem(item);
+        }
+
         return "redirect:/items";
+    }
+
+    @RequestMapping("/remove/{id}")
+    public String removeItem(@PathVariable("id") int id) {
+        this.itemService.removeItem(id);
+
+        return "redirect:/items";
+    }
+
+    @RequestMapping("edit/{id}")
+    public String editItem(@PathVariable("id") int id, Model model) {
+        model.addAttribute("item", this.itemService.getItemById(id));
+        model.addAttribute("listItems", this.itemService.listItems());
+
+        return "items";
+    }
+
+    @RequestMapping("itemdata/{id}")
+    public String itemData(@PathVariable("id") int id, Model model) {
+        model.addAttribute("item", this.itemService.getItemById(id));
+
+        return "itemdata";
     }
 }
 
