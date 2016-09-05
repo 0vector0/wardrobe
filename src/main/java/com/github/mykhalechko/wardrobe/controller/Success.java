445a274c63e3caee47d2b3b65e12431a5b7ca050
0vector0 <0vector0@gmail.com>
2016-09-05 16:25:51 +0300

    save all

--- src/main/java/com/github/mykhalechko/wardrobe/controller/Success.java
+++ src/main/java/com/github/mykhalechko/wardrobe/controller/Success.java
@@ -1,27 +1,27 @@
-package com.github.mykhalechko.wardrobe.controller;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
-
-/**
- * Created by vector on 17.08.2016.
- */
-@WebServlet("/success")
-public class Success extends HttpServlet {
-
-    protected void doGet(HttpServletRequest request,
-                         HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter writer = response.getWriter();
-
-        writer.println("" + "" + "<center>"
-                + "Details Added Successfully" + "</center>" + ""
-                + "");
-    }
-
-}
-
+//package com.github.mykhalechko.wardrobe.controller;
+//
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+///**
+// * Created by vector on 17.08.2016.
+// */
+//@WebServlet("/success")
+//public class Success extends HttpServlet {
+//
+//    protected void doGet(HttpServletRequest request,
+//                         HttpServletResponse response) throws ServletException, IOException {
+//        PrintWriter writer = response.getWriter();
+//
+//        writer.println("" + "" + "<center>"
+//                + "Details Added Successfully" + "</center>" + ""
+//                + "");
+//    }
+//
+//}
+//
--- src/main/java/com/github/mykhalechko/wardrobe/controller/UserControllerServlet.java
+++ src/main/java/com/github/mykhalechko/wardrobe/controller/UserControllerServlet.java
@@ -1,47 +1,47 @@
-package com.github.mykhalechko.wardrobe.controller;
-
-import com.github.mykhalechko.wardrobe.dao.UserDAO;
-
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-import java.io.IOException;
-
-
-@WebServlet("/register")
-public class UserControllerServlet extends HttpServlet {
-
-    private static final long serialVersionUID = 1L;
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String nextJsp = "/register.jsp";
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJsp);
-        dispatcher.forward(request, response);
-    }
-
-    protected void doPost(HttpServletRequest request,
-                          HttpServletResponse response) throws ServletException, IOException {
-
-        String userName = request.getParameter("userName");
-        String password = request.getParameter("password1");
-        String email = request.getParameter("email");
-        String phone = request.getParameter("phone");
-        String city = request.getParameter("city");
-
-        HttpSession session = request.getSession(true);
-        try {
-            UserDAO userDAO = new UserDAO();
-            userDAO.addUserDetails(userName, password, email, phone, city);
-            response.sendRedirect("success");
-        } catch (Exception e) {
-
-            e.printStackTrace();
-        }
-
-    }
-}
+//package com.github.mykhalechko.wardrobe.controller;
+//
+//import com.github.mykhalechko.wardrobe.dao.UserDAO;
+//
+//import javax.servlet.RequestDispatcher;
+//import javax.servlet.ServletException;
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import javax.servlet.http.HttpSession;
+//import java.io.IOException;
+//
+//
+//@WebServlet("/register")
+//public class UserControllerServlet extends HttpServlet {
+//
+//    private static final long serialVersionUID = 1L;
+//
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        String nextJsp = "/register.jsp";
+//        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(nextJsp);
+//        dispatcher.forward(request, response);
+//    }
+//
+//    protected void doPost(HttpServletRequest request,
+//                          HttpServletResponse response) throws ServletException, IOException {
+//
+//        String userName = request.getParameter("userName");
+//        String password = request.getParameter("password1");
+//        String email = request.getParameter("email");
+//        String phone = request.getParameter("phone");
+//        String city = request.getParameter("city");
+//
+//        HttpSession session = request.getSession(true);
+//        try {
+//            UserDAO userDAO = new UserDAO();
+//            userDAO.addUserDetails(userName, password, email, phone, city);
+//            response.sendRedirect("success");
+//        } catch (Exception e) {
+//
+//            e.printStackTrace();
+//        }
+//
+//    }
+//}
